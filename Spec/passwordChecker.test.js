602c32db7925c23ec8d967e3b6f4a011d9abe82d
Diallo Ibrahima <ibrahima.k.diallo@student.vinci.be>
2025-01-17 17:03:54 +0100

    add the function has1SpecialCharacter

--- Spec/passwordChecker.test.js
+++ Spec/passwordChecker.test.js
@@ -9,13 +9,27 @@ describe('Password Checker', () => {
     it('should return false if password is less than 8 characters', () => {
         
 
-        expect(result.has8Caracters('8573249')).toBe(false);
+        expect(result.has8Characters('8573249')).toBe(false);
     });
 
     it('should return true if password is 8 characters', () => {
         
 
-        expect(result.has8Caracters('85732495')).toBe(true);
+        expect(result.has8Characters('85732495')).toBe(true);
     });
 
+    // Step 2
+
+    it('should return false if password does not have a special character', () => {
+        
+
+        expect(result.has1SpecialCharacter('84621325')).toBe(false);
+    });
+
+    it('should return true if password has a special character', () => {
+        
+
+        expect(result.has1SpecialCharacter('dd44@d4e')).toBe(true);
+    });
+    
 });
--- Src/passwordChecker.js
+++ Src/passwordChecker.js
@@ -1,5 +1,16 @@
 export class Main {
-    has8Caracters(password) {
+    has8Characters(password) {
         return password.length >= 8;
+    }
+
+    has1SpecialCharacter(password) {
+        const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '='];
+
+        for (let i = 0; i < password.length; i++) {
+            if (specialCharacters.includes(password[i])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
